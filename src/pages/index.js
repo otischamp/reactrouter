e741f93bf4a9245b6a0044f252067f933c56eecf
Othmane Merzak <otischamp@gmail.com>
2024-03-27 13:54:20 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Layout from "./Layout";
+import Home from "./Home";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
+import NoPage from "./NoPage";
+
+export {Layout, Home, Blogs, Contact, NoPage}
